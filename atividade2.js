65e0db283a03bc640dc06e36415b3393e79fd269
yasminnanes <yasminnanes@outlook.com>
2022-03-13 13:34:27 -0300

    atividade presencial 13/03

---
+++ atividade2.js
@@ -0,0 +1,10 @@
+const alunos = 14;
+for(let x = 0; x<=alunos; x++){
+    if(x ==0){
+        console.log("Número zero");
+    }else if(x %2 ==0){
+        console.log("O número é par:" + x);
+    }else{
+        console.log("O Número é impar:" + x);
+    }
+}
